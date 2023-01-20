37707be47c94c146bac5faccc3df9a237574693d
scratchminer <121195139+scratchminer@users.noreply.github.com>
2023-01-20 03:52:37 -0500

    Add a bulk-decompile script for convenience

---
+++ unluac.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# unluac.sh
+# A shell script to make unluac.jar easier to use. (Still requires the JRE, though)
+
+if [ $# -eq 0 ]; then
+	echo "Use this shell script to bulk-decompile Playdate Lua bytecode with unluac."
+	echo "Usage: ./unluac.sh [-r] (input directory) [separate output directory]"
+	echo
+	echo "If '-r' is given, remove the input bytecode files after processing."
+	echo "If there is no output directory given, create code in the input directory."
+	echo
+	exit
+fi
+
+if [ "$1" = "-r" ]; then
+	REMOVE=1
+else
+	REMOVE=0
+fi
+
+if [ $REMOVE -eq 0 ]; then
+	INPUTDIR="$1"
+	OUTPUTDIR="$2"
+else
+	INPUTDIR="$2"
+	OUTPUTDIR="$3"
+fi
+
+if [ -z "$OUTPUTDIR" ]; then OUTPUTDIR=$INPUTDIR; fi
+IFS=$'\n'
+for LUACFILE in $(find "$INPUTDIR" -name '*.luac'); do
+	
+	# Get the name of the new source file
+	LUAFILE=$(echo "${LUACFILE//$INPUTDIR/$OUTPUTDIR}" | sed 's+.luac+.lua+gI')
+	
+	# If the folder containing it doesn't exist, create it
+	[ ! -f $LUAFILE ] && mkdir -p $(dirname "$LUAFILE")
+	
+	# Run unluac
+	java -jar $(dirname $0)/unluac.jar $LUACFILE > $LUAFILE
+	
+	# Remove the old bytecode file if '-r' was passed
+	[ $REMOVE -eq 1 ] && rm -f $LUAFILE
+done
+unset IFS
